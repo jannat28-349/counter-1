d36fc3b093534289e20c37a6dce90b91e76c9567
jannat28-349 <jucse2.349@gmail.com>
2023-01-18 01:44:14 +0600

    added grade calculator

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-let count = 0;
+let marks = 0;
+let grade = "";
 
-function setCount() {
-  document.getElementById("output").innerText = count;
+
+function setGrade() {
+  document.getElementById("output").innerText = `Your grade is: ${grade}`;
 }
 
-function setInitialCount() {
-  setCount();
+function gradeCalculate() {
+  marks = document.querySelector("#marks").value;
+  if(marks<0 || marks>100) {
+    document.getElementById("output").innerText = `Enter valid marks`;
+    return;
+  }
+  else if(marks < 33) {
+    grade = "F";
+  }
+  else if(marks <= 39) {
+    grade = "D";
+  }
+  else if(marks <= 49) {
+    grade = "C";
+  }
+  else if(marks <= 59) {
+    grade = "B";
+  }
+  else if(marks <= 69) {
+    grade = "A-";
+  }
+  else if(marks <= 79) {
+    grade = "A";
+  }
+  else {
+    grade = "A+";
+  }
+  setGrade();
 }
 
-function increment() {
-  count++;
-  setCount();
-}
-
-setInitialCount();
