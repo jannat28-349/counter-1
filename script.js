ab2158df4ba82777df4d16d995d077fd1f901209
jannat28-349 <jucse2.349@gmail.com>
2023-01-18 21:55:43 +0600

    Build List Done

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-let marks = 0;
-let grade = "";
+// const d = document.getElementById("list");
 
+// d.innerHTML = "<p> Hello </p>";
 
-function setGrade() {
-  document.getElementById("output").innerText = `Your grade is: ${grade}`;
+// const btn = document.getElementsByTagName("button")[0];
+// btn.style.backgroundColor = "blue";
+// btn.style.padding = "10px 20px";
+// btn.style.borderRadius = "30px";
+// btn.style.color = "white";
+
+function addElement() {
+  const d = document.getElementById("list");
+  const e = document.createElement("h1");
+  const text = document.getElementById("item").value;
+  e.innerText = text;
+  d.appendChild(e);
 }
-
-function gradeCalculate() {
-  marks = document.querySelector("#marks").value;
-  if(marks<0 || marks>100) {
-    document.getElementById("output").innerText = `Enter valid marks`;
-    return;
-  }
-  else if(marks < 33) {
-    grade = "F";
-  }
-  else if(marks <= 39) {
-    grade = "D";
-  }
-  else if(marks <= 49) {
-    grade = "C";
-  }
-  else if(marks <= 59) {
-    grade = "B";
-  }
-  else if(marks <= 69) {
-    grade = "A-";
-  }
-  else if(marks <= 79) {
-    grade = "A";
-  }
-  else {
-    grade = "A+";
-  }
-  setGrade();
-}
-
